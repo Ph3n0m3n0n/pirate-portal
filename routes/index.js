21003a2a3a417e32ac63a006b67d52b75a66272d
Erza Scarlet <Erza Scarlet>
2016-12-22 09:38:05 -0600

    added a few dependencies and some links to the navbar. Also updated the README.md file.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var mongo = require('mongodb');
+var session = require('express-session');
+var User = require('../models/users.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	User.find({}, function (err, docs) {
+    if (err) throw err;
+      // console.log(docs);
+      res.render('index', { title: 'Pirate Portal', docs: docs });
+  	  console.log('This is the index.js file')
+  });
 });
 
 module.exports = router;
