ff2990958a996f5deeb5522b747aa12cbc0df3aa
Ph3n0m3n0n <kantaris96@gmail.com>
2016-12-23 00:27:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,8 +24,10 @@ var app = express();
 // Database stuff ... =/
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
+console.log("Using Mongo: " + process.env.MONGO_URL);
 var db = mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/userlist');
-var User = require('./models/users')
+var User = require('./models/users');
+var Post = require('./models/posts');
 // just to make sure that I am actually connecting...
 mongoose.connection.once('connected', function() {
   console.log("Connected to database")
@@ -36,6 +39,13 @@ app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(session({
+  secret: 'shhhhhhhhhhhhhhhhhh',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,6 +55,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(validator());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
