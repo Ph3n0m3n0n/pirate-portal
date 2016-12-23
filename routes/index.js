4296f05ea8ee277473d8b91d6e3380279129f649
Erza Scarlet <Erza Scarlet>
2016-12-23 09:16:00 -0600

    NOT WORKING

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ var Post = require('../models/posts.js');
 
 // GET Index featured articles
 router.get('/', function(req, res, next) {
-  console.log('current user is ' + req.user.name);
+  // console.log('current user is ' + req.user.name);
   Post.find({}, function(err, posts) {
     if (err) throw err;
-      // console.log(docs);
+      console.log(docs);
     res.render('index', { title: 'Pirate Portal', posts: posts });
   });
 });
